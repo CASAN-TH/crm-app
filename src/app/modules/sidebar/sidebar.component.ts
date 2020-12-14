aa123570a3e64282be3ded40f684d94cde1c0e1c
kriangkrai <kriangkrai86@gmail.com>
2020-12-14 18:25:19 +0700

    binding

--- src/app/modules/sidebar/sidebar.component.ts
+++ src/app/modules/sidebar/sidebar.component.ts
@@ -6,7 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-
+icon=[
+  {
+    "name":"Detail",
+    "icon": '../../'
+  },
+  {
+    "name":"Contract",
+    "icon":"../../../assets/icon/icon_menu/contract.svg"
+  },
+  {
+    "name":"Companies",
+    "icon":"../../../assets/icon/icon_menu/companies.svg"
+  },
+  {
+    "name":"Product",
+    "icon":"../../../assets/icon/icon_menu/product.svg"
+  },
+  {
+    "name":"Activity",
+    "icon":"../../../assets/icon/icon_menu/activity.svg"
+  },
+  {
+    "name":"Dashboards",
+    "icon":"../../../assets/icon/icon_menu/dashboard.svg"
+  },
+]
   constructor() { }
 
   ngOnInit(): void {
