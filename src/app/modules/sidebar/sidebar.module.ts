32f61cc205b3e7ec16f4a8605bc2d7e54af07533
kriangkrai <kriangkrai86@gmail.com>
2020-12-14 17:38:48 +0700

    mat-list

--- src/app/modules/sidebar/sidebar.module.ts
+++ src/app/modules/sidebar/sidebar.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar.component';
-
+import {MatListModule} from '@angular/material/list';
 
 
 @NgModule({
   declarations: [SidebarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatListModule
   ],
   exports:[SidebarComponent]
 })
